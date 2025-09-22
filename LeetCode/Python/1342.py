fc0ab90e1e7dc8171d2da20b7b33ce1e85e49411
HwangGoeun <hwang_gim@naver.com>
2025-09-22 10:50:24 +0900

    파이썬 폴더 생성

---
+++ LeetCode/Python/1342.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        cnt = 0         # count
+        while num > 0:
+            if num & 1:
+                num -= 1
+            else:
+                num >>= 1
+            cnt += 1
+        
+        return cnt
