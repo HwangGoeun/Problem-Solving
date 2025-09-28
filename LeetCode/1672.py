d30009c6b9540674ffa21ef43ff91959800f96ff
HwangGoeun <hwang_gim@naver.com>
2025-09-28 10:19:22 +0900

    디렉토리 구조 변경

--- LeetCode/1672.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        max = 0
-        for i in range(len(accounts)):
-            for j in range(len(accounts[0])):
-                if max < sum(accounts[i]):
-                    max = sum(accounts[i])
-        
-        return max
